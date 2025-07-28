3743fcde19b0bf48dbcdc78cea75072438389386
Ayush Raj <rajayush3182003@gmail.com>
2025-07-28 22:55:09 +0530

    Second Commit

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,16 @@
-export default function blog(){
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog",
+    },
+}
+
+export default async function blog(){
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Intentional Delay")
+        }, 2000)
+    })
     return <h1>My blog</h1>
 }
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,26 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string}>;
+}
+
+export const generateMetadata = async({
+  params,
+} : Props): Promise<Metadata> =>{
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Andriod ${id}`)
+    }, 100)
+  })
+  return{
+    title:`Product ${title}`
+  }
+}
+
 export default async function ProductDetails({
   params,
-}: {
-  params: Promise<{ productId: string }>;
-}) {
+}: Props) {
   const productId = (await params).productId;
   return <h1>Details About Product {productId}</h1>;
 }
