9afc0b53d237262766ed80791be411da0c2f0dd0
Ayush Raj <rajayush3182003@gmail.com>
2025-08-05 13:27:50 +0530

    Streaming

---
+++ rendering-demo/src/app/product-reviews/page.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import { Product } from "../components/product";
+import { Reviews } from "../components/reviews";
+
+export default function ProductReviews() { 
+  return (
+    <div>
+      <h1>Product Reviews</h1>
+
+      <Suspense fallback={<p>Loading product details.....</p>}>
+        <Product />
+      </Suspense>
+
+      <Suspense fallback={<p>Loading revies.....</p>}>
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
