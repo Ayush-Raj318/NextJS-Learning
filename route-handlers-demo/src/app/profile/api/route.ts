9d325ef133ad5a4df146ff83373479e163057248
Ayush Raj <rajayush3182003@gmail.com>
2025-08-05 10:36:04 +0530

    Headers and Cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,25 @@
-export async function GET(){
-    return new Response("Profile API data")
+import { headers, cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(cookieStore.get('resultPerPage'));
+  
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
